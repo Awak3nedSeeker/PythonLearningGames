60fb261d6c46de031ac064ce55fd395eaf316f54
Awak3nedSeeker <Javier_Ramirez.20@Yahoo.com>
2021-08-12 20:37:18 -0400

    Fixed Closing error

--- PythonLearning/PythonLearning/PythonPong.py
+++ PythonLearning/PythonLearning/PythonPong.py
@@ -9,9 +9,14 @@ Win.bgcolor("blue")
 Win.setup(width=800, height=600)
 Win.tracer(0)
 
+Canvas = Win.getcanvas()
+Root = Canvas.winfo_toplevel()
+
 def Quit():
     global Running
     Running = False
+
+Root.protocol("WM_DELETE_WINDOW", Quit)
 
 Win.listen()
 Win.onkeypress(Quit, "q")
